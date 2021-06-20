07612d16c6a67a16a1a0f9904d40ab693fa259ae
Paula Gómez <paulagomez@MacBook-Air-de-Paula.local>
2021-06-19 19:42:04 -0500

    Menu

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,79 @@
 <template>
-  <div id="app">
-   <Home v-if="false"></Home>
-   <Pixabay></Pixabay>
-  </div>
+<v-app>
+<v-system-bar
+  height="30"
+>
+<v-app-bar-nav-icon @click.stop="mostrarMenu=!mostrarMenu">
+
+</v-app-bar-nav-icon>
+</v-system-bar>
+  <v-navigation-drawer
+      v-model="mostrarMenu"
+        expand-on-hover
+        temporary
+        absolute
+        :mini-variant="miniVariant"
+      >
+        <v-list>
+          <v-list-item class="px-2">
+            <v-list-item-avatar>
+              <v-img src="./assets/logo.png"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+
+          <v-list-item link>
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+              Curso Vue
+              </v-list-item-title>
+              <v-list-item-subtitle>Pauli</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list nav dense>
+        <div link v-for="m in menu" :key="m.nombre">
+        <router-link :to="m.ruta">
+        <v-list-item link>
+          <v-list-item-icon>
+            <v-icon>
+              {{m.icono}}
+            </v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>{{m.nombre}}</v-list-item-title>
+          </v-list-item>
+        </router-link>
+        </div>
+        </v-list>
+      </v-navigation-drawer>
+      <router-view></router-view>
+</v-app>
 </template>
 
 <script>
-import Home from "./components/Home"
-import Pixabay from "./components/pixabay/Pixabay"
-
+// import Home from "./components/Home"
+// import Pixabay from "./components/pixabay/Pixabay"
 export default {
   name: 'App',
   components: {
-    Home,
-    Pixabay
+    // Home,
+    // Pixabay
+  },
+  data(){
+    return{
+      menu:[
+        {ruta:'/',nombre:'Inicio',icono:'mdi-folder'},
+         {ruta:'/pixa',nombre:'Imagenes',icono:'mdi-folder'},
+      ],
+      mostrarMenu:false,
+      miniVariant:true
+
+    }
   }
 }
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+
+import vuetify from './plugins/vuetify'
+import Vuetify from 'vuetify'
+
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
 import { BootstrapVue } from 'bootstrap-vue'
+
+import router from './router'
+
 
 
 Vue.use(BootstrapVue)
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
